009d8ec73eb043ac3100bc44a0c7b521ab1cbe96
inotbatya <93325881+inotbatya@users.noreply.github.com>
2024-11-28 00:42:09 +0700

    Update and rename somonecod.py to train_model.py

--- train_model.py
+++ train_model.py
@@ -6,7 +6,9 @@ import joblib
 
 # Загрузка данных
 iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=42
+)
 
 # Обучение модели
 model = RandomForestClassifier(random_state=42)
